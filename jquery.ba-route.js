24194ef6e8ecdb891ff3931bff54acfeb6e9757d
Ben Alman <cowboy@rj3.net>
2011-09-16 08:07:14 -0400

    Changed unnamed wildcards to splats, a la Backbone. Except this still returns a params object instead of having to call a function passing in matches in-order.

--- jquery.ba-route.js
+++ jquery.ba-route.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Route - v0.1pre - 9/15/2011
+ * JavaScript Basic Route Matcher - v0.1pre - 9/15/2011
  * http://benalman.com/
  *
  * Copyright (c) 2011 "Cowboy" Ben Alman
@@ -7,57 +7,54 @@
  * http://benalman.com/about/license/
  */
 
-function getRoute(route, url) {
-  // Param names, in order
-  var paramNames = [];
-  // The route parsing function to be returned (or invoked if a url is passed).
-  var fn;
-  
-  if (typeof route === "string") {
-    // Build route RegExp from passed string.
-    route = route.replace(/(\*)|:([^\/]+)/g, function(r, s, param) {
-      if (param) {
-        // Replace named :param with non-greedy capture group.
-        paramNames.push(param);
-        return "([^/]+)";
-      } else {
-        // Replace * wildcard with greedy capture group.
-        paramNames.push(false);
-        return "(.*)";
-      }
-    });
+(function(global) {
+  // Characters to be escaped with \.
+  var reEscape = /[-[\]{}()+?.,\\^$|#\s]/g;
+  // Match :xxx or *xxx param placeholders.
+  var reParam = /([:*])(\w+)/g;
 
-    // Create RegExp from parsed route string, adding anchors and escaping
-    // as-necessary.
-    route = new RegExp("^" + route.replace(/\//g, "\\/") + "$");
+  global.getRoute = function(route, url) {
+    // Param names, in order
+    var names = [];
+    // The route parsing function to be returned (or invoked if a url was
+    // passed).
+    var fn;
 
-    fn = function(url) {
-      var i, key;
-      var params = {};
-      var matches = url.match(route);
-      // If no matches, return null.
-      if (!matches) { return null; }
-      // Loop over all :params / * wildcards.
-      for (i = 0; i < paramNames.length; i++) {
-        // Set the key var to the stored :param name or `false` if wildcard.
-        if (!(key = paramNames[i])) {
-          // Wildcard. Get the numeric key.
-          key = params.length || 0;
-          // Increment the length property.
-          params.length = key + 1;
+    if (typeof route === "string") {
+      // Build route RegExp from passed string, replacing any :param or *splat
+      // with an appropriate capture group.
+      route = route.replace(reEscape, "\\$&");
+      route = route.replace(reParam, function(_, mode, name) {
+        names.push(name);
+        // :param captures until the next /, while *splat captures until the
+        // next :param, *splat, or EOL.
+        return mode === ":" ? "([^/]*)" : "(.*)";
+      });
+
+      // Add ^/$ anchors and escape as-necessary. RegExp borrowed from the
+      // Backbone.js router.
+      route = new RegExp("^" + route + "$");
+
+      fn = function(url) {
+        var i = 0;
+        var params = {};
+        var matches = url.match(route);
+        // If no matches, return null.
+        if (!matches) { return null; }
+        // Add all :params / * wildcards values into the params object.
+        while(i < names.length) {
+          params[names[i++]] = matches[i];
         }
-        // Set the new value (:param name or numeric index for wildcard) in the
-        // params object.
-        params[key] = matches[i + 1];
-      }
-      return params;
-    };
-  } else {
-    // RegExp route was passed. This is super-simple.
-    fn = function(url) {
-      return url.match(route);
-    };
-  }
-  // If a url was passed, return matches, otherwise return a matching function.
-  return url == null ? fn : fn(url);
-}
+        return params;
+      };
+    } else {
+      // RegExp route was passed. This is super-simple.
+      fn = function(url) {
+        return url.match(route);
+      };
+    }
+    // If a url was passed, return params or matches, otherwise return the
+    // route-matching function.
+    return url == null ? fn : fn(url);  };
+
+}(this.exports || this));
--- unit/jquery.ba-route.js
+++ unit/jquery.ba-route.js
@@ -1,70 +1,86 @@
 
 test("called with url", function() {
-  same(getRoute("/users", "/foo"), null, "shouldn't match");
-  same(getRoute("/users", "/users"), {}, "should match");
+  same(getRoute("users", "foo"), null, "shouldn't match");
+  same(getRoute("users", "users"), {}, "should match");
 });
 
 test("called without url", function() {
-  var r = getRoute("/users");
+  var r = getRoute("users");
   same(typeof r, "function", "should return a function");
-  same(r("/foo"), null, "shouldn't match");
-  same(r("/users"), {}, "should match");
+  same(r("foo"), null, "shouldn't match");
+  same(r("users"), {}, "should match");
 });
 
 test("regex route", function() {
-  var r = getRoute(/^\/users?(?:\/(\d+)(?:\.\.(\d+))?)?/);
-  same(r("/foo"), null, "shouldn't match");
-  same(r("/user"), ["/user", undefined, undefined], "should match");
-  same(r("/user/123"), ["/user/123", "123", undefined], "should match");
-  same(r("/user/123..456"), ["/user/123..456", "123", "456"], "should match");
+  var r = getRoute(/^users?(?:\/(\d+)(?:\.\.(\d+))?)?/);
+  same(r("foo"), null, "shouldn't match");
+  same(r("user"), ["user", undefined, undefined], "should match");
+  same(r("user/123"), ["user/123", "123", undefined], "should match");
+  same(r("user/123..456"), ["user/123..456", "123", "456"], "should match");
 });
 
 test("string route, basic", function() {
-  var r = getRoute("/users");
-  same(r("/fail"), null, "shouldn't match");
-  same(r("/users/"), null, "shouldn't match");
-  same(r("/users/foo"), null, "shouldn't match");
-  same(r("/users"), {}, "Should match");
+  var r = getRoute("users");
+  same(r("fail"), null, "shouldn't match");
+  same(r("users/"), null, "shouldn't match");
+  same(r("users/foo"), null, "shouldn't match");
+  same(r("users"), {}, "Should match");
 });
 
 test("string route, one variable", function() {
-  var r = getRoute("/users/:id");
-  same(r("/users"), null, "shouldn't match");
-  same(r("/users/"), null, "shouldn't match");
-  same(r("/users/123/456"), null, "shouldn't match");
-  same(r("/users/123"), {id: "123"}, "should match");
+  var r = getRoute("users/:id");
+  same(r("users"), null, "shouldn't match");
+  same(r("users/123/456"), null, "shouldn't match");
+  same(r("users/"), {id: ""}, "should match");
+  same(r("users/123"), {id: "123"}, "should match");
 });
 
 test("string route, multiple variables", function() {
-  var r = getRoute("/users/:id/:other");
-  same(r("/users"), null, "shouldn't match");
-  same(r("/users/123"), null, "shouldn't match");
-  same(r("/users/123/456"), {id: "123", other: "456"}, "should match");
+  var r = getRoute("users/:id/:other");
+  same(r("users"), null, "shouldn't match");
+  same(r("users/123"), null, "shouldn't match");
+  same(r("users/123/456"), {id: "123", other: "456"}, "should match");
 });
 
-test("string route, one wildcard", function() {
-  var r = getRoute("/users/*");
-  same(r("/users"), null, "shouldn't match");
-  same(r("/users/"), {length: 1, 0: ""}, "should match");
-  same(r("/users/123"), {length: 1, 0: "123"}, "should match");
-  same(r("/users/123/456"), {length: 1, 0: "123/456"}, "should match");
+test("string route, one splat", function() {
+  var r = getRoute("users/*stuff");
+  same(r("users"), null, "shouldn't match");
+  same(r("users/"), {stuff: ""}, "should match");
+  same(r("users/123"), {stuff: "123"}, "should match");
+  same(r("users/123/456"), {stuff: "123/456"}, "should match");
 });
 
-test("string route, multiple wildcards", function() {
-  var r = getRoute("/users/*/*");
-  same(r("/users"), null, "shouldn't match");
-  same(r("/users/123"), null, "shouldn't match");
-  same(r("/users//"), {length: 2, 0: "", 1: ""}, "should match");
-  same(r("/users//123"), {length: 2, 0: "", 1: "123"}, "should match");
-  same(r("/users///123"), {length: 2, 0: "/", 1: "123"}, "should match");
-  same(r("/users/123/456"), {length: 2, 0: "123", 1: "456"}, "should match");
-  same(r("/users/123/456/789"), {length: 2, 0: "123/456", 1: "789"}, "capturing should be greedy");
+test("string route, multiple splats", function() {
+  var r = getRoute("users/*stuff/*more");
+  same(r("users"), null, "shouldn't match");
+  same(r("users/123"), null, "shouldn't match");
+  same(r("users/123/"), {stuff: "123", more: ""}, "should match");
+  same(r("users//123"), {stuff: "", more: "123"}, "should match");
+  same(r("users//"), {stuff: "", more: ""}, "should match");
+  same(r("users///123"), {stuff: "/", more: "123"}, "should match");
+  same(r("users/123/456"), {stuff: "123", more: "456"}, "should match");
+  same(r("users/123/456/789"), {stuff: "123/456", more: "789"}, "capturing should be greedy");
 });
 
-test("string route, variables and wildcards", function() {
-  var r = getRoute("/users/:id/*/:other/*");
-  same(r("/users/123/aaa/456/bbb"), {id: "123", other: "456", length: 2, 0: "aaa", 1: "bbb"}, "this is pushing it");
+test("string route, variables and splats", function() {
+  var r = getRoute("users/:id/*stuff/:other/*more");
+  same(r("users/123/aaa/456/bbb"), {id: "123", other: "456", stuff: "aaa", more: "bbb"}, "this is pushing it");
 
-  r = getRoute("/users/:id/:other/*/*");
-  same(r("/users/123/456/aaa/bbb/ccc"), {id: "123", other: "456", length: 2, 0: "aaa/bbb", 1: "ccc"}, "this is a little more reasonable");
+  r = getRoute("users/:id/:other/*stuff/*more");
+  same(r("users/123/456/aaa/bbb/ccc"), {id: "123", other: "456", stuff: "aaa/bbb", more: "ccc"}, "this is a little more reasonable");
+});
+
+// These were pulled from the backbone.js unit tests.
+test("a few backbone.js test routes", function() {
+  r = getRoute("search/:query/p:page");
+  same(r("search/boston/p20"), {query: "boston", page: "20"}, "should match");
+
+  r = getRoute("*first/complex-:part/*rest");
+  same(r("one/two/three/complex-part/four/five/six/seven"), {first: "one/two/three", part: "part", rest: "four/five/six/seven"}, "should match");
+
+  r = getRoute(":entity?*args");
+  same(r("cowboy?a=b&c=d"), {entity: "cowboy", args: "a=b&c=d"}, "should match");
+
+  r = getRoute("*anything");
+  same(r("doesnt-match-a-route"), {anything: "doesnt-match-a-route"}, "should match");
 });
